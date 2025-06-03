412d95785ea8bd60bd3b019382b1edbc7eba0a55
fallleaves01 <adoubiq@qq.com>
2025-06-03 18:28:40 +0800

    feat: 修改创建商品页面

--- src/views/Product/CreateProduct/CreateProduct.vue
+++ src/views/Product/CreateProduct/CreateProduct.vue
@@ -1,177 +1,165 @@
-
-
-
 <template>
-        <!-- 面包屑导航区 -->
-<el-breadcrumb >
+  <!-- 面包屑导航区 -->
+  <el-breadcrumb>
     <el-breadcrumb-item :to="{ path: '/homePage' }">首页</el-breadcrumb-item>
     <el-breadcrumb-item>创建课程</el-breadcrumb-item>
-</el-breadcrumb>
-<!-- 返回按钮 -->
+  </el-breadcrumb>
+  <!-- 返回按钮 -->
 
 
-<div>
-      <el-row :gutter="20" style="margin-top:10px;">
+  <div>
+    <el-row :gutter="20" style="margin-top:10px;">
 
-    <el-col :span="16">
-        <div >
-       <el-card >
-
+      <el-col :span="16">
         <div>
+          <el-card>
 
-        <el-form label-width="80px" v-model="courseForm" size="small" label-position="right">
-          <el-form-item label="课程名" prop="">
-            <el-input  auto-complete="off" v-model="courseForm.courseName" ></el-input>
-          </el-form-item>
-          <el-form-item label="课程简介" prop="">
-            <el-input  auto-complete="off" v-model="courseForm.courseInfo" ></el-input>
-          </el-form-item>
-          <el-form-item label="课程封面" prop="">
-            
-            <el-upload action="#" ref="upload" list-type="picture-card" :auto-upload="false" :file-list="fileList" :limit="1" :http-request="picUpload">
-            <el-icon><Plus /></el-icon>
+            <div>
 
-              <template #file="{ file }">
-              <div>
-                <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
-                <span class="el-upload-list__item-actions">
-                  <span
-                    class="el-upload-list__item-preview"
-                    @click="handlePictureCardPreview(file)"
-                  >
-                    <el-icon><zoom-in /></el-icon>
-                  </span>
-                  <!-- <span
-                    v-if="!disabled"
-                    class="el-upload-list__item-delete"
-                    @click="handleDownload(file)"
-                  >
-                    <el-icon><Download /></el-icon>
-                  </span> -->
-                  <span
-                    v-if="!disabled"
-                    class="el-upload-list__item-delete"
-                    @click="handleRemove(file)"
-                  >
-                    <el-icon><Delete /></el-icon>
-                  </span>
-                </span>
+              <el-form label-width="100px" v-model="productForm" size="small" label-position="right">
+                <el-form-item label="商品名" prop="">
+                  <el-input auto-complete="off" v-model="productForm.productName"></el-input>
+                </el-form-item>
+                <el-form-item label="商品简介" prop="">
+                  <el-input auto-complete="off" v-model="productForm.productInfo"></el-input>
+                </el-form-item>
+                <el-form-item label="开始拍卖时间" prop="startAuctionTime">
+                  <el-date-picker
+                    v-model="productForm.startAuctionTime"
+                    type="datetime"
+                    placeholder="选择开始拍卖时间"
+                    style="width: 100%;"
+                  ></el-date-picker>
+                </el-form-item>
+                <!-- 添加起拍价 -->
+                <el-form-item label="起拍价(元)" prop="startPrice">
+                  <el-input-number
+                    v-model="productForm.startPrice"
+                    :min="0"
+                    style="width: 100%;"
+                  />
+                </el-form-item>
+                <!-- 添加拍卖持续时间 -->
+                <el-form-item label="拍卖持续时间(小时)" prop="durationHours">
+                  <el-input-number
+                    v-model="productForm.durationHours"
+                    :min="1"
+                    style="width: 100%;"
+                  />
+                </el-form-item>
+                <el-form-item label="商品图片" prop="">
 
+                  <el-upload action="#" ref="upload" list-type="picture-card" :auto-upload="false" :file-list="fileList" :limit="1">
+                    <template #trigger v-if="fileList.length === 0">
+                      <el-icon style="font-size:32px;cursor:pointer;">
+                        <Plus />
+                      </el-icon>
+                    </template>
+                    <template #file="{ file }">
+                      <div>
+                        <img class="el-upload-list__item-thumbnail" :src="file.url" alt="" />
+                        <span class="el-upload-list__item-actions">
+                          <span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
+                            <el-icon><zoom-in /></el-icon>
+                          </span>
+                          <span v-if="!disabled" class="el-upload-list__item-delete" @click="handleRemove(file)">
+                            <el-icon>
+                              <Delete />
+                            </el-icon>
+                          </span>
+                        </span>
+
+                      </div>
+                    </template>
+                  </el-upload>
+
+                  <el-dialog v-model="dialogVisible">
+                    <img w-full :src="dialogImageUrl" alt="Preview Image" />
+                  </el-dialog>
+
+
+
+                </el-form-item>
+              </el-form>
+
+              <!-- <template #file="{ file }"> -->
+              <div slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="createproduct">创建课程</el-button>
               </div>
-            </template>
-          </el-upload>
+              <!-- </template> -->
+            </div>
+          </el-card>
+        </div>
+      </el-col>
 
-          <el-dialog v-model="dialogVisible">
-            <img w-full :src="dialogImageUrl" alt="Preview Image" />
-          </el-dialog>
-
-
-
-          </el-form-item>
-        </el-form>
-
-        <!-- 章节信息 -->
-        <h3>课程章节</h3>
-        <div class="create-course">
-    <div v-for="(chapter, index) in chapters" :key="index">
-      <div style="display: flex;">
-        <div style="flex: 0.3;">
-          <el-input v-model="chapter.chapterNo" placeholder="章节号" />
-        </div>
-        <div style="flex: 0.7;">
-          <el-input v-model="chapter.chapterName" placeholder="章节名" />
-        </div>
-        <div style="flex: 0.6;">
-          <el-button @click="removeChapter(index)" type="danger"  circle>
-            <el-icon style="vertical-align: middle;">
-              <Delete />
-            </el-icon>
-          </el-button>
-        </div>
-      </div>
-    </div>
-    <el-button @click="addChapter" type="success"  circle >
-      <el-icon style="vertical-align: middle;">
-      <Plus />
-    </el-icon>
-    </el-button>
-    <!-- <button @click="printcha">输出</button> -->
-        </div>
-        <!-- <template #file="{ file }"> -->
-        <div slot="footer" class="dialog-footer">
-             <el-button type="primary" @click="createproduct">创建课程</el-button> 
-        </div>
-      <!-- </template> -->
-        </div>
-      </el-card>
-        </div>
-    </el-col>
-
-      </el-row>
-</div>
+    </el-row>
+  </div>
 </template>
 <script>
 
-import {createproduct,UploadPic} from '@/utils/api/CreateClassApi'
+import { CreateProduct } from '@/utils/api/CreateProductApi'
 
-import { Delete, Download, Plus, ZoomIn ,Check} from '@element-plus/icons-vue'
-export default{
-    data(){
-        return{
-          fileList: [],
-          dialogImageUrl: '',
+import { Delete, Download, Plus, ZoomIn, Check } from '@element-plus/icons-vue'
+export default {
+  data() {
+    return {
+      fileList: [],
+      dialogImageUrl: '',
       dialogVisible: false,
       disabled: false,
-            courseForm:{
-                courseName:'',
-                courseInfo:'',
-            },
-            chapters: [],
-            courseId:null,
-        };
+      productForm: {
+        productName: '',
+        productInfo: '',
+        startAuctionTime: null,
+        // 添加起拍价字段
+        startPrice: null,
+        // 添加拍卖持续时间字段
+        durationHours: null,
+      },
+      chapters: [],
+      courseId: null,
+    };
+  },
+  methods: {
+    createproduct() {
+      var that = this;
+      let formData = new FormData();
+      formData.append("product_name", that.productForm.productName);
+      formData.append("descreption", that.productForm.productInfo);
+      formData.append("start_price", that.productForm.startPrice);
+      formData.append("due_time", that.productForm.durationHours);
+      formData.append("plan_start_time", that.productForm.startAuctionTime);
+      if (that.fileList[0] && that.fileList[0].raw) {
+        formData.append("image", that.fileList[0].raw);
+      }
+      CreateProduct(formData).then(function (resp) {
+        console.log(resp)
+        if (resp.data.status === 200) {
+          that.courseId = resp.data.courseId
+          that.$message.success("商品创建成功，id为:" + resp.data.courseId)
+          that.fileList = [];
+        }
+        else {
+          that.$message.error(resp.data.msg);
+        }
+      });
     },
-    methods:{
-        createproduct()
-        {
-            var that = this;
-
-            let data = {
-
-                "courseName": that.courseForm.courseName,
-                "courseInfo": that.courseForm.courseInfo,
-                "chapter":that.chapters
-            }
-            createproduct(data).then(function(resp){
-                    console.log(resp)
-                    if(resp.data.status === 200)
-                    {
-                        that.courseId=resp.data.courseId
-                        that.$message.success("课程创建成功，课程id为:"+resp.data.courseId)
-                        that.$refs.upload.submit();
-                        that.fileList = [];
-                    }
-                    else {
-                        that.$message.error(resp.data.msg);
-                    }
-            });
-        },
-        backallproduct()
-        {
-            this.$router.push('/allproduct')
-        },
-        addChapter() {
-          this.chapters.push({ chapterNo: null, chapterName: '' });
-        },
-        removeChapter(index) {
-          this.chapters.splice(index, 1);
-        },
-        // printcha() {
-        //   this.chapters.forEach(function (item) {
-        //     console.log(item.number);
-        //     console.log(item.name);
-        //   });
-        // }
-        handleRemove(file) {
+    backallproduct() {
+      this.$router.push('/allproduct')
+    },
+    addChapter() {
+      this.chapters.push({ chapterNo: null, chapterName: '' });
+    },
+    removeChapter(index) {
+      this.chapters.splice(index, 1);
+    },
+    // printcha() {
+    //   this.chapters.forEach(function (item) {
+    //     console.log(item.number);
+    //     console.log(item.name);
+    //   });
+    // }
+    handleRemove(file) {
       console.log(file)
       this.fileList = [];
 
@@ -183,37 +171,26 @@ export default{
     handleDownload(file) {
       console.log(file)
     },
-    picUpload(f)
-    {
-      let vm=this
-      let params = new FormData()
-      params.append("file", f.file);
-      params.append("courseId",vm.courseId)
-      UploadPic(params).then(function(resp){
-        if(resp.data.status==200)
-        {
-          vm.$message.success("封面上传成功,课程id为："+vm.courseId)
-        }
-      })
-    }
-    }
+  }
 }
 
 </script>
 <style>
 .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
+  border-radius: 4px;
+  min-height: 36px;
 }
+
 .bg-purple {
-    background: #d3dce6;
-  }
-  
-  .box-card {
-    width: 100%;
-  }
-  .dialog-footer{
-  padding-top:10px ;
+  background: #d3dce6;
+}
+
+.box-card {
+  width: 100%;
+}
+
+.dialog-footer {
+  padding-top: 10px;
   padding-left: 10%;
 }
 </style>
