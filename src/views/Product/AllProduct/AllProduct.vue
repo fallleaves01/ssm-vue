ea2c6a6d15835d423dcde3b8e92ef819aa7421b0
fallleaves01 <fallleaves01@outlook.com>
2025-06-05 20:31:57 +0800

    feat: 添加加入拍卖接口

--- src/views/Product/AllProduct/AllProduct.vue
+++ src/views/Product/AllProduct/AllProduct.vue
@@ -6,55 +6,56 @@
   </el-breadcrumb>
 
   <div class="product-home">
- <div>
-    <div >
-            <el-row>
-                <el-col :span="16">
-                    <el-input v-model="keyWord" placeholder="请输入关键词"></el-input>
-                </el-col>
-                <el-col :span="8">
-                    <el-button @click="search" circle>
-                    <el-icon style="vertical-align: middle;">
-                      <Search />
-                    </el-icon>
-                    </el-button>
-                </el-col>
-            </el-row>
-        </div>
-  </div>
-  <!-- 全部课程列表 -->
-  <div class="product-list">
-    <el-row :gutter="20">
-      <el-col :span="8" v-for="product in productList" :key="product.id">
-        <el-card class="product-card" shadow="hover">
-          <div slot="header">
-            <img :src="product.image" alt="商品图片" class="product-cover" />
-          </div>
-          <div class="product-info">
-            <h2>{{ product.product_name }}</h2>
-            <p>{{ product.description }}</p>
-          </div>
-          <div class="product-footer">
-            <el-button type="success" @click="buyproduct(course.product_id) " v-if="true">加入竞拍</el-button>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
-  </div>
+    <div>
+      <div>
+        <el-row>
+          <el-col :span="16">
+            <el-input v-model="keyWord" placeholder="请输入关键词"></el-input>
+          </el-col>
+          <el-col :span="8">
+            <el-button @click="search" circle>
+              <el-icon style="vertical-align: middle;">
+                <Search />
+              </el-icon>
+            </el-button>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <!-- 全部商品列表 -->
+    <div class="product-list">
+      <el-row :gutter="20">
+        <el-col :span="8" v-for="product in productList" :key="product.id">
+          <el-card class="product-card" shadow="hover">
+            <div slot="header">
+              <img :src="product.image" alt="商品图片" class="product-cover" />
+            </div>
+            <div class="product-info">
+              <h2>{{ product.product_name }}</h2>
+              <p>{{ product.description }}</p>
+            </div>
+            <div class="product-footer">
+              <el-button type="success" @click="buyproduct(product.product_id)"
+                v-if="!buyer_auction_pro_id.has(product.product_id)">加入竞拍</el-button>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
 
   </div>
   <!-- 搜索框 -->
- 
+
 </template>
 
 <script>
-import { GetAllCourseList,SearchCourse } from '@/utils/api/AllClassApi'
-import {buyProduct} from '@/utils/api/JoinClassApi'
+import { GetAllCourseList, SearchCourse } from '@/utils/api/AllClassApi'
+// import {buyProduct} from '@/utils/api/JoinClassApi'
 import { GetTotalProductList, SearchTotalProductList } from '@/utils/api/ProductApi'
 import { GetBuyerAuctionList, EnterAuction } from '@/utils/api/AuctionApi'
 import { GetUserInfo } from '@/utils/api/UserApi'
 
-import { Search} from '@element-plus/icons-vue'
+import { Search } from '@element-plus/icons-vue'
 export default {
   data() {
     return {
@@ -66,56 +67,77 @@ export default {
         auction_list: []
       },
       buyer_auction_pro_id: new Set(),
-      keyWord:''
+      keyWord: ''
     }
   },
   created() {
     let vm = this
     let data = {}
     try {
-      GetUserInfo()
+      // 先获取用户信息，在获取到用户名后再调用其他依赖用户名的API
+      GetUserInfo().then(function (resp) {
+        vm.username = resp.data.user_name  // 确保正确获取user_name字段
+        console.log("获取到用户名:", vm.username)
+        // 在获取到用户名后调用需要用户名的API
+        return GetBuyerAuctionList(vm.username)
+      }).then(function (resp) {
+        console.log("获取拍卖列表成功:", resp)
+        vm.buyer_auction.product_list = resp.data.product_list;
+        vm.buyer_auction.auction_list = resp.data.auction_list;
+        // 使用for...of循环正确迭代数组元素
+        for (let item of vm.buyer_auction.product_list) {
+          vm.buyer_auction_pro_id.add(item.product_id);
+        }
+      }).catch(function (error) {
+        console.error("获取用户信息或拍卖列表失败:", error)
+      })
+
+      // 商品列表获取不依赖用户名，可以并行请求
       GetTotalProductList().then(function (resp) {
         console.log(resp)
         if (resp.data.product_list !== null) {
-          resp.data.product_list.forEach((data) => {
-            vm.productList.push(data)
-          })
+          vm.productList = resp.data.product_list; // 直接赋值数组，避免forEach每次push
         }
+      }).catch(function (error) {
+        console.error("获取商品列表失败:", error)
       })
-      
     } catch (error) {
       console.log(error)
     }
-  },
-  methods: {
+  }, methods: {
     // createCourse() {
     //   this.$router.push("/allproduct/createproduct")
     // }
-    buyproduct(courseId)
-    {
+    buyproduct(productId) {
       let that = this;
-            let data = {
-                "courseId": courseId
-            }
-            buyProduct(data).then(function(resp){
-                    console.log(resp)
-                    if(resp.data.status === 200)
-                    {
-                        that.$message.success(resp.data.msg)
-                    }
-                    else {
-                        that.$message.error(resp.data.msg);
-                    }
-            });
-    },
-    search()
-    {
-      let vm=this
-      let data={
-        "keyWord":vm.keyWord
-      }
-      SearchCourse(data).then(function(resp){
-        vm.courseList=resp.data.courseList
+      EnterAuction(productId).then(function (resp) {
+        console.log(resp)
+        switch (resp.data.status) {
+          case 0:
+            that.$message.success("您已成功加入竞拍")
+            break;
+          case 1:
+            that.$message.error("商品拍卖未开始");
+            break;
+          case 2:
+            that.$message.error("商品拍卖已结束");
+            break;
+          case 3:
+            that.$message.error("商品不存在");
+            break;
+          default:
+            that.$message.error("未知错误，请稍后再试");
+            break;
+        }
+      });
+    }, search() {
+      let vm = this
+      SearchTotalProductList(vm.keyWord).then(function (resp) {
+        if (resp.data.product_list !== null) {
+          vm.productList = resp.data.product_list;
+        }
+      }).catch(function (error) {
+        console.error("搜索产品失败:", error);
       })
     }
   }
@@ -148,8 +170,13 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 2; /* 最多显示2行 */
+  display: box;
+  -webkit-line-clamp: 2;
+  /* 最多显示2行 */
+  line-clamp: 2;
+  /* 标准属性 */
   -webkit-box-orient: vertical;
+  box-orient: vertical;
   white-space: normal;
 }
 
@@ -157,13 +184,16 @@ export default {
   margin: 20px 0;
   text-align: center;
 }
+
 .product-footer {
   margin-top: auto;
-  margin-bottom: 10px; /* 调整底部距离 */
+  margin-bottom: 10px;
+  /* 调整底部距离 */
   display: flex;
   justify-content: space-evenly;
   align-items: center;
 }
+
 /* 根据需要添加其他样式 */
 .product-home {
   display: flex;
